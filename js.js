a4b3f04e291545ef93c1ab4a426d0272b7672712
PavelSidyuk <76783639+PavelSidyuk@users.noreply.github.com>
2023-11-24 15:18:47 +0400

    1

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
 
 const password = document.querySelector('#password');
+
 const toggleVisibility = document.querySelector('.password__toggle');
 let state = new Map([
     ['password', 'text'],
@@ -8,8 +9,15 @@ let state = new Map([
 
 toggleVisibility.addEventListener('click', () => {
     password.setAttribute('type', state.get(password.getAttribute('type')))
-});
 
+});
+const password_reg = document.querySelector('#register_password');
+const regToggleVisibility = document.querySelector('.register_password__toggle');
+
+regToggleVisibility.addEventListener('click', () => {
+    password_reg.setAttribute('type', state.get(password_reg.getAttribute('type')))
+
+});
 
 const formSwitch = document.querySelector('.form__switch')
 const tabs =  document.querySelectorAll('.form__switch_btn');
